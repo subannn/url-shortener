d2cef3673c5b7dcac19fb619d5ee06694c28f9fb
subannn <ibraimov534@gmail.com>
2024-06-10 23:07:01 +0600

    removed useless print

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -14,7 +13,6 @@ import (
 )
 
 func main() {
-	fmt.Println(os.Getenv("REDIS_ADDRESS"))
 	mtx := &sync.Mutex{}
 	redis.RunRedis(mtx)
 
